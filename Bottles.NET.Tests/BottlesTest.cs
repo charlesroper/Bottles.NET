75f9361a047b39d665d354a82a387853d61c8e15
Charles Roper <charles@roper.im>
2017-03-12 23:36:14 +0000

    Add second failing test

--- Bottles.NET.Tests/BottlesTest.cs
+++ Bottles.NET.Tests/BottlesTest.cs
@@ -19,11 +19,19 @@ namespace Bottles.NET.Tests
 
             var expected = @"
 99 bottles of beer on the wall, 99 bottles of beer.
-Take one down and pass it around, 98 bottles of beer on the wall.
-".Trim();
+Take one down and pass it around, 98 bottles of beer on the wall.".Trim();
 
-            _output.WriteLine("Verse 99: {0}", sut);
-            _output.WriteLine("Expected: {0}", expected);
+            Assert.Equal(expected, sut);
+        }
+
+        [Fact]
+        public void TestAnotherVerse()
+        {
+            var sut = Bottles.Verse(89);
+
+            var expected = @"
+89 bottles of beer on the wall, 89 bottles of beer.
+Take one down and pass it around, 88 bottles of beer on the wall.".Trim();
 
             Assert.Equal(expected, sut);
         }
