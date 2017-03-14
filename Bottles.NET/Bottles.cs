b05e9b032cc9acc64652005692b5bf6dfc9b7551
Charles Roper <charles@roper.im>
2017-03-14 10:41:15 +0000

    Refactor to be more consice and readable

--- Bottles.NET/Bottles.cs
+++ Bottles.NET/Bottles.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Bottles.NET
 {
@@ -12,35 +7,31 @@ namespace Bottles.NET
         public string Verse(int currentVerse)
         {
             int nextVerse = currentVerse - 1;
-            var lineOne = string.Empty;
-            var lineTwo = string.Empty;
+            var verse = "";
 
             if (currentVerse > 2)
             {
-                lineOne = $"{currentVerse} bottles of beer on the wall, {currentVerse} bottles of beer.{Environment.NewLine}";
-                lineTwo = $"Take one down and pass it around, {nextVerse} bottles of beer on the wall.";
-
+                verse = $"{currentVerse} bottles of beer on the wall, {currentVerse} bottles of beer.{Environment.NewLine}" +
+                        $"Take one down and pass it around, {nextVerse} bottles of beer on the wall.";
             }
 
             if (currentVerse == 2)
             {
-                lineOne = $"{currentVerse} bottles of beer on the wall, {currentVerse} bottles of beer.{Environment.NewLine}";
-                lineTwo = $"Take one down and pass it around, {nextVerse} bottle of beer on the wall.";
+                verse = $"{currentVerse} bottles of beer on the wall, {currentVerse} bottles of beer.{Environment.NewLine}" +
+                        $"Take one down and pass it around, {nextVerse} bottle of beer on the wall.";
             }
 
             if (currentVerse == 1)
             {
-                lineOne = $"{currentVerse} bottle of beer on the wall, {currentVerse} bottle of beer.{Environment.NewLine}";
-                lineTwo = $"Take it down and pass it around, no more bottles of beer on the wall.";
+                verse = $"{currentVerse} bottle of beer on the wall, {currentVerse} bottle of beer.{Environment.NewLine}" +
+                         "Take it down and pass it around, no more bottles of beer on the wall.";
             }
 
             if (currentVerse == 0)
             {
-                lineOne = $"No more bottles of beer on the wall, no more bottles of beer.{Environment.NewLine}";
-                lineTwo = "Go to the store and buy some more, 99 bottles of beer on the wall.";
+                verse = $"No more bottles of beer on the wall, no more bottles of beer.{Environment.NewLine}" +
+                         "Go to the store and buy some more, 99 bottles of beer on the wall.";
             }
-            
-            var verse = lineOne + lineTwo;
 
             return verse;
         }
